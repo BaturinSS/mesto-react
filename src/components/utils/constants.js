10e81ae67879655af8d239227bd15897e1a5a7fe
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-05-04 21:08:33 +0300

    + utils

---
+++ src/components/utils/constants.js
@@ -0,0 +1,13 @@
+export const profileOpenPopupButtonEdit = document.querySelector('.profile__button-edit');
+
+export const formEditProfile = document.querySelector('.popup__form_type_profile-edit');
+
+export const profileOpenPopupButtonAdd = document.querySelector('.profile__button-add');
+
+export const formAddCard = document.querySelector('.popup__form_type_card-add');
+
+export const selectorTemplate = '#elements-template';
+
+export const avatarOpenPopupButtonEdit = document.querySelector('.profile__ellipse');
+
+export const formEditAvatar = document.querySelector('.popup__form_type_avatar-edit');
