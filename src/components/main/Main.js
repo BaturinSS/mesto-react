093b6052bca8064000091c21e582c2aa395e34ba
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-05-05 01:57:56 +0300

    completed api GET profile

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Main.css';
+import { api } from '../utils/Api';
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+
+  useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getCards()])
+      .then(([userData, cards]) => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+      })
+      .catch((err) => {
+        err.then((res) => {
+          alert(res.message)
+        })
+      })
+  }, []);
+
   return (
     <div className="content">
       <section className="profile">
         <div className="profile__figure">
-          <div className="profile__ellipse" onClick={onEditAvatar}><img className="plofile__avatar" src="#" alt="аватар" /></div>
+          <div className="profile__ellipse" onClick={onEditAvatar}><img className="plofile__avatar" src={userAvatar} alt="аватар" /></div>
           <div className="profile__info">
             <div className="profile__group">
-              <h1 className="profile__name">Жак-Ив-Кусто</h1>
+              <h1 className="profile__name">{userName}</h1>
               <button type="button" className="profile__button-edit" onClick={onEditProfile}></button>
             </div>
-            <p className="profile__subtitle">Исследователь океана</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </div>
         <button type="button" className="profile__button-add" onClick={onAddPlace}></button>
