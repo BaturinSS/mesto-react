6d02831598f45dcd0acc3bcda1654cd7afa6c1fe
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-08-31 02:27:23 +0300

    ++

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <p className="footer__copyright">
-        © 2022 - {new Date().getFullYear()}. Батурин Сергей
+        © {new Date().getFullYear()}. Батурин Сергей
       </p>
     </div>
-  )
+  );
 }
 
 export default Footer;
